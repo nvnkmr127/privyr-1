2ca0379d13f711bf65690d41831d91730daa745d
Naveen Kumar Adicharla <39632585+nvnkmr127@users.noreply.github.com>
2026-08-13 10:23:31 +0530

    Remove duplicate CustomField relationship

--- laravel-crm/packages/Webkul/Moldable/src/Models/Workspace.php
+++ laravel-crm/packages/Webkul/Moldable/src/Models/Workspace.php
@@ -4,7 +4,6 @@ namespace Webkul\Moldable\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Webkul\Moldable\Models\WorkspaceMember;
 
 class Workspace extends Model
 {
@@ -20,11 +19,6 @@ class Workspace extends Model
     public function teams(): HasMany
     {
         return $this->hasMany(Team::class);
-    }
-
-    public function fields(): HasMany
-    {
-        return $this->hasMany(CustomField::class);
     }
 
     public function views(): HasMany
