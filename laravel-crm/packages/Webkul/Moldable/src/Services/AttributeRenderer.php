b9a174a2571887867a9c4dc8de83bae161f58bd2
Naveen <nvnkmr127@gmail.com>
2026-08-13 10:49:28 +0530

    feat: implement attribute renderer service and dynamic blade component for various input types

---
+++ laravel-crm/packages/Webkul/Moldable/src/Services/AttributeRenderer.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Webkul\Moldable\Services;
+
+use Illuminate\Support\Facades\View;
+use Webkul\Attribute\Models\Attribute;
+use Webkul\Attribute\Models\AttributeValue;
+
+class AttributeRenderer
+{
+    /**
+     * Render an attribute dynamic input field HTML.
+     *
+     * @param  Attribute  $attribute
+     * @param  mixed  $value
+     * @param  mixed  $entity
+     * @return string
+     */
+    public static function render(Attribute $attribute, mixed $value = null, mixed $entity = null): string
+    {
+        if ($value instanceof AttributeValue) {
+            $value = $value->value;
+        } elseif ($value === null && $entity && isset($entity->{$attribute->code})) {
+            $value = $entity->{$attribute->code};
+        }
+
+        return View::make('moldable::components.attribute-renderer', [
+            'attribute' => $attribute,
+            'value'     => $value,
+            'entity'    => $entity,
+        ])->render();
+    }
+}
