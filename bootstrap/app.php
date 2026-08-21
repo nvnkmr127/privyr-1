d0a0695c11a9794ec6173fabe0d0154e35404fb0
nvnkmr127 <39632585+nvnkmr127@users.noreply.github.com>
2026-08-21 11:56:53 +0000

    chore: applied pint changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureCaptureSecret;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(CanInstall::class);
 
         $middleware->alias([
-            'capture.secret' => \App\Http\Middleware\EnsureCaptureSecret::class,
+            'capture.secret' => EnsureCaptureSecret::class,
         ]);
 
         $middleware->encryptCookies(except: [
